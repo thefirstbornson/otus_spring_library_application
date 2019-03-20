3c55297bbb025450461115e735e3b833080b1bfc
Artem <thefirstbornson@gmail.com>
2019-03-20 10:54:15 +0200

    hw5 dao author test corrected

--- src/test/java/ru/otus/dao/AuthorDaoJDBCImplTest.java
+++ src/test/java/ru/otus/dao/AuthorDaoJDBCImplTest.java
@@ -1,5 +1,6 @@
 package ru.otus.dao;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,13 +10,12 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.shell.jline.InteractiveShellApplicationRunner;
 import org.springframework.shell.jline.ScriptShellApplicationRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import ru.otus.domain.Author;
-
-import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 //@SpringBootTest(properties = {
@@ -29,6 +29,8 @@ public class AuthorDaoJDBCImplTest {
 
     private static final String NAME = "Fedor";
     private static final String SURNAME = "Dostoevsky";
+    private static final String NAME1 = "Viktor";
+    private static final String SURNAME1 = "Pelevin";
 
     @Autowired
     AuthorDao authorDataJDBC;
@@ -51,24 +53,40 @@ public class AuthorDaoJDBCImplTest {
 
     @Test
     public void saveTest(){
+        authorDataJDBC.save(author);
+        Assert.assertTrue(author.getId()>0);
+    }
+
+    @Test(expected = EmptyResultDataAccessException.class)
+    public void notFoundByIdTest(){
+        Author result = authorDataJDBC.findById(author.getId());
+    }
+
+    @Test
+    public void findByIdTest(){
+        authorDataJDBC.save(author);
+        Author result = authorDataJDBC.findById(author.getId());
+        Assert.assertTrue(author.getAuthor_first_name().equals(NAME));
+        Assert.assertTrue(author.getAuthor_last_name().equals(SURNAME));
         System.out.println(authorDataJDBC.save(author));
     }
+
 
     @Test
     public void updateTest(){
         authorDataJDBC.save(author);
-        author.setAuthor_first_name("Artem");
-        author.setAuthor_last_name("Poskrebyshev");
+        author.setAuthor_first_name(NAME1);
+        author.setAuthor_last_name(SURNAME1);
         authorDataJDBC.update(author);
+        Assert.assertTrue(author.getAuthor_first_name().equals(NAME1));
+        Assert.assertTrue(author.getAuthor_last_name().equals(SURNAME1));
         System.out.println(author);
     }
 
-    @Test
+    @Test(expected = EmptyResultDataAccessException.class)
     public void deleteTest(){
         authorDataJDBC.save(author);
-        System.out.println("Try to delete "+author);
         authorDataJDBC.delete(author);
+        authorDataJDBC.findById(author.getId());
     }
-
-
 }
