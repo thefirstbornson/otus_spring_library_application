49b358aa32054e3c8f9c9e638a7b4a990bad1228
Artem <thefirstbornson@gmail.com>
2019-04-11 15:28:00 +0200

    hw7 final

--- src/main/java/ru/otus/dao/AuthorDao.java
+++
@@ -1,6 +0,0 @@
-package ru.otus.dao;
-
-import ru.otus.domain.Author;
-
-public interface AuthorDao extends GenericDao<Author>{
-}
--- src/main/java/ru/otus/dao/BookCommentDao.java
+++
@@ -1,6 +0,0 @@
-package ru.otus.dao;
-
-import ru.otus.domain.BookComment;
-
-public interface BookCommentDao extends GenericDao<BookComment> {
-}
