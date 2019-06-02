e2cacd9183b6f630b18aad8bd5965caa3eff5815
Artem <thefirstbornson@gmail.com>
2019-06-02 13:38:05 +0300

    hw10 no front final

--- src/main/java/ru/otus/config/AppConfig.java
+++ src/main/java/ru/otus/config/AppConfig.java
@@ -1,4 +1,4 @@
-package ru.otus.controller.config;
+package ru.otus.config;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
--- src/main/java/ru/otus/config/SwaggerConfig.java
+++ src/main/java/ru/otus/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package ru.otus.controller.config;
+package ru.otus.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
